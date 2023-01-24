d851d88601c15350d1bc611ede37fffeee4cce36
Bishwonath <112367927+Bishwonath@users.noreply.github.com>
2023-01-24 09:20:40 +0545

    Add files via upload

---
+++ turtle23.py
@@ -0,0 +1,64 @@
+# # # import turtle
+# # # b=turtle.Screen()
+# # # t=turtle.Turtle()
+# # # turtle.mainloop()
+
+
+# # '''import turtle
+# # t=turtle.Turtle()
+# # t.shape("circle")  # arrow turtle circle square triangle classic
+# # turtle.mainloop()'''
+
+
+# # # import turtle
+# # # t=turtle.Turtle()
+# # # t.shape("turtle")
+# # # t.color("red","green")
+# # # t.forward(200)
+# # # turtle.done()
+
+# # import turtle
+# # sc=turtle.Screen()
+# # sc.screensize(1500,600)
+# # sc.bgcolor("black")
+# # sc.title("Turtle")
+# # t=turtle.Turtle()
+# # t.speed(1)./
+# # t.forward(0)
+
+# # t.shape("turtle")
+# # t.color("red","green")
+# # t.forward(100)
+
+# # t.clear()
+# # turtle.done()
+
+
+
+# import turtle
+# t=turtle.Turtle()
+# t.fd(90)
+# t.rt(90)
+# t.fd(90)
+# t.rt(90)
+# t.fd(90)
+# t.rt(90)
+# t.fd(90)
+# t.rt(90)
+# t.hideturtle()
+# turtle.mainloop()
+
+# import turtle
+# wn=turtle.Screen()
+# pen=turtle.Turtle()
+# pen.begin_fill()
+# pen.fillcolor("black")
+# for i in range(10):
+#     pen.fd(51.42)
+#     pen.rt(36)
+# pen.end_fill()
+# pen.hideturtle()
+# turtle.done()
+
+import turtle
+t = turtle.Turtle
